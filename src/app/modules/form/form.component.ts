a907a9b4598cdfeac5ef2ada46fd1431e9ed31b6
vevis <vevisvs@gmail.com>
2023-07-09 13:32:40 -0300

    creacion de la interface user e inyeccion del FormBuilder en el constructor

--- src/app/modules/form/form.component.ts
+++ src/app/modules/form/form.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+
+interface User{
+  name: FormControl<string | null>,
+  lastname: FormControl<string | null>,
+  email: FormControl<string | null>,
+  password: FormControl<string | null>
+}
 
 @Component({
   selector: 'app-form',
@@ -6,5 +14,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
-
+  user: FormGroup<User>;
+  constructor(private builder: FormBuilder){
+    this.user = this.builder.group({
+      name: [''],
+      lastname: [''],
+      email: [''],
+      password: ['']
+    })
+  }
 }
