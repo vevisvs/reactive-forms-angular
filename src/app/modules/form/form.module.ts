a2ab4531949e4619d10102c14a6586c8f2e86aa5
vevis <vevisvs@gmail.com>
2023-07-09 01:35:29 -0300

    estructura formulario, estilos generales

--- src/app/modules/form/form.module.ts
+++ src/app/modules/form/form.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,11 @@ import { FormComponent } from './form.component';
     FormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    FormComponent
   ]
 })
 export class FormModule { }
